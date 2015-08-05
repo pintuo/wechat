af7b56f8112f8e27b808574750b234bc168f88dc
nextsteve <dotai@qq.com>
2015-08-05 17:20:24 +0800

    setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,17 +59,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+ #       'ENGINE': 'django.db.backends.mysql',
+  #      'NAME': 'mysite',
+   #     'USER':'root',
+    #    'PASSWORD':'bdyun',
+     #   'HOST':'',
+	#    'PORT':'',
+    #}
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysite',
-        'USER':'root',
-        'PASSWORD':'bdyun',
-        'HOST':'',
-	    'PORT':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
